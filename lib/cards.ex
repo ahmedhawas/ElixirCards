b87d0c12d29c06b2dd1719ce5d5d821818f14019
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-11-06 19:14:08 -0500

    Created create_deck function

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,7 +1,12 @@
 defmodule Cards do
   # module is a collection of functions
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suit <- suits do # generates a nested loop
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
